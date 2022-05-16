b84c2914bcc3352663f185dec15a41a1819910ca
Mysteryneon <63605642+Mysteryneon@users.noreply.github.com>
2022-05-16 10:25:37 +0300

    Created the Post list screen

--- App.js
+++ App.js
@@ -3,27 +3,37 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import axios from 'axios';
+
 import HomeScreen from './components/HomeScreen.js';
 import UserPage from './components/UserPage.js';
+import PostsScreen from './components/PostsScreen.js';
+import PostPage from './components/PostPage.js';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   const [users, setUsers] = React.useState();
+  const [posts, setPosts] = React.useState();
 
   React.useEffect(() => {
     axios
       .get('https://jsonplaceholder.typicode.com/users')
       .then((res) => setUsers(res.data))
       .catch((err) => alert(err));
-   }, []);
 
-
+    axios
+      .get('https://jsonplaceholder.typicode.com/posts')
+      .then((res) => setPosts(res.data))
+      .catch((err) => alert(err));
+  }, []);
 
   return (
     <NavigationContainer>
       <StatusBar backgroundColor="#10A9B0" barStyle="light-content" />
       <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="PostsScreen">
+          {(props) => <PostsScreen {...props} data={posts} />}
+        </Stack.Screen>
         <Stack.Screen name="Home">
           {(props) => <HomeScreen {...props} data={users} />}
         </Stack.Screen>
@@ -33,7 +43,20 @@ export default function App() {
               key={user.id}
               name={`${user.id}${user.name.replace(/ /g, '-')}`}
             >
-              {(props) => <UserPage {...props} data={user} />}
+              {(props) => <PostPage {...props} data={user} />}
+            </Stack.Screen>
+          );
+        })}
+
+        {posts?.map((post) => {
+          return (
+            <Stack.Screen
+              key={post.id}
+              name={`${post.id}${
+                post.title.replace(/ /g, '-').split(0, 24)[0]
+              }`}
+            >
+              {(props) => <UserPage {...props} data={post} />}
             </Stack.Screen>
           );
         })}
---
+++ components/PostPage.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const PostPage = () => {
+  return (
+    <View>
+      <Text>PostPage</Text>
+    </View>
+  )
+}
+
+export default PostPage
+
+const styles = StyleSheet.create({})
